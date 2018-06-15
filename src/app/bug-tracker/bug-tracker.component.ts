fa27484154a4e3d7c5a435c31f9b986a48f8a465
Laxman Venkatesh <laxman@Laxmans-Air.Dlink>
2018-06-16 03:14:37 +0530

    Local Storage CHanges

--- src/app/bug-tracker/bug-tracker.component.ts
+++ src/app/bug-tracker/bug-tracker.component.ts
@@ -22,22 +22,35 @@ export class BugTrackerComponent implements OnInit {
 
 
   ngOnInit() {
+    let windowStorageLength: number = window.localStorage.length;
+    for(let i=0; i<windowStorageLength; i++){
+      this.bugs[i] = JSON.parse(window.localStorage.getItem( window.localStorage.key(i)));
+    }
   }
 
   onClickAddNew(bugName: string){
     
-    let bug:BugModel = this.bugServices.createNew(bugName);
+    let bugId = 'bug'+(window.localStorage.length+1);
+    let bug:BugModel = this.bugServices.createNew(bugId, bugName);
     this.bugs.push(bug)
+    
+    window.localStorage.setItem(bug.bugId,JSON.stringify(bug));
+    //window.localStorage.set
   }
 
   onClickBugName(bug: BugModel){
     bug.isClosed = !bug.isClosed;
+    let newBug: BugModel = bug;
+    window.localStorage.setItem(bug.bugId,JSON.stringify(newBug));
     
   }
 
   onClickRemoveClosed(){
+    let closedBugs: BugModel[] = this.bugs.filter(bug=>bug.isClosed);
     this.bugs = this.bugs.filter(bug=>!bug.isClosed);
-   
+    closedBugs.forEach(bug => {
+      window.localStorage.removeItem(bug.bugId);
+    });
   }
 
   getClosedBugCount(): number{
